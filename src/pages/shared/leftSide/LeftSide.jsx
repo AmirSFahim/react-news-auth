4b6b0825d443fad69fe2a180006cadea88a52c97
amir96-eb <amir_ali_96@yahoo.com>
2023-11-05 21:45:46 +0600

    adding left side components

--- src/pages/shared/leftSide/LeftSide.jsx
+++ src/pages/shared/leftSide/LeftSide.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const LeftSide = () => {
+    const [catagories, setCatagories] = useState([]);
+
+
+    useEffect(()=>{
+        fetch('categories.json')
+        .then( res => res.json())
+        .then( data => setCatagories(data))
+    }
+        
+        ,[])
+
     return (
-        <div>
-            <h2 className="text-2xl">Left side</h2>
+        <div className="space-y-6">
+            <h2 className="text-2xl">Left side {catagories.length}</h2>
+            {
+                catagories.map(category => <Link 
+                    to={`/category/${category.id}`}
+                    className="block ml-4 font-semibold px-4 " 
+                key={category.id}>{category.name}</Link>)
+            }
         </div>
     );
 };
